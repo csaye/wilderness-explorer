b698136cc98e1ffc825ab0d191126d37c6baee0c
Cooper Saye <coopersaye@gmail.com>
2021-02-17 16:41:11 -0700

    Create main game loop.

--- WildernessExplorer.py
+++ WildernessExplorer.py
@@ -14,3 +14,22 @@ screen = pygame.display.set_mode(screen_size)
 # set caption
 pygame.display.set_caption('Wilderness Explorer')
 
+### main ###
+
+clock = pygame.time.Clock()
+frame = 0
+
+# game loop
+while True:
+
+    # tick
+    clock.tick(60)
+    frame += 1
+
+    # events
+    for event in pygame.event.get():
+
+        # quit
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
